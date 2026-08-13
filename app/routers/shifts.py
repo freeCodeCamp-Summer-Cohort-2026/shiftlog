923912fa0743df7b6df1e89b00b18adce9f8b398
Ekomjah Edet <ekomjahedet@gmail.com>
2026-08-13 22:56:58 +0100

    add limiters to put and delete endpoints

--- app/routers/shifts.py
+++ app/routers/shifts.py
@@ -161,6 +161,7 @@ def get_shift(shift_id: int, session: Session = Depends(get_session)):
 
 
 @router.delete("/{shift_id}", status_code=204)
+@limiter.limit("10/30seconds")
 def delete_shift(shift_id: int, session: Session = Depends(get_session)):
     """
     DELETE request:
